01f500be691af89af942e99512306b8391e970dd
phhung190101 <phhung190101@gmail.com>
2023-03-18 23:45:56 +0700

    WIP: products table

--- database/migrations/2023_03_14_161738_create_products_table.php
+++ database/migrations/2023_03_14_161738_create_products_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,6 +16,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->text('name');
+            $table->text('slug')->nullable();
+            $table->jsonb('price_with_color')->default(DB::raw("('{}')"));
+            $table->jsonb('price_with_config')->default(DB::raw("('{}')"));
+            $table->jsonb('thumb')->default(DB::raw("('{}')"));
+            $table->jsonb('details')->default(DB::raw("('{}')"));
+            $table->string('producer')->nullable();
+            $table->text('source')->nullable();
+            $table->float('rate')->nullable()->default('0');
+            $table->text('reviews')->nullable();
             $table->timestamps();
         });
     }
