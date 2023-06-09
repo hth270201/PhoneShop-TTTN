5f6691d77f839f7a999f481fdbdd081c9bd363e9
phhung190101 <phhung190101@gmail.com>
2023-06-10 01:05:04 +0700

    WIP: vnpay

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use App\Models\Color;
 use App\Models\Product;
+use App\Services\VNPayServices;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -12,6 +14,8 @@ class ProductController extends Controller
     {
         $top_product = Product::orderBy('rate', 'DESC')->orderBy('price', 'DESC')->limit(10)->get();
         $releated_product = Product::orderBy('price', 'DESC')->limit(10)->get();
+        VNPayServices::create();
+        dump($_GET);
         return view('pages.home', [
             'top_product' => $top_product,
             'releated_prodct' => $releated_product
